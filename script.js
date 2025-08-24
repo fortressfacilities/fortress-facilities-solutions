45103164cee88cb2cd9e19f100561465427b88c7
fortressfacilities <ryan@axiommanagement.co>
2025-08-23 17:22:41 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-
-function fakeSubmit(form){
-  const btn = form.querySelector('button[type="submit"]');
-  btn.disabled = true; btn.textContent = 'Sending…';
-  setTimeout(()=>{
-    alert('Thanks! We will reach out shortly.');
-    btn.disabled = false; btn.textContent = 'Request Quote';
-    form.reset();
-  }, 800);
-}
+/* reserved for enhancements */
